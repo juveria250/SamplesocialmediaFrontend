38099952d279e173ebbdaaf496b6c6f6f79965fe
juveria250 <121285120+juveria250@users.noreply.github.com>
2023-02-07 12:26:06 +0530

    Add files via upload

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+import img4 from "../img/img4.jpg";
+
+export const Followers = [
+  { name: "Divyani Shirke", username: "ShirkeDivyani", img: img1 },
+  { name: "Abida Lambe", username: "AbidaLambe", img: img2 },
+  { name: "Madiha Lambe", username: "LambeMadia", img: img3 },
+  { name: "Sumera", username: "Sumera", img: img4 },
+];
---
+++ src/Data/PostsData.js
@@ -0,0 +1,29 @@
+import postPic1 from '../img/painting1.jpg'
+import postPic2 from '../img/painting2.jpg'
+import postPic3 from '../img/painting3.jpg'
+
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'Abida',
+        desc: "I was immediately drawn to this piece.",
+        likes: 2300,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: 'Madiha',
+        desc: "I love how you used color in this piece.",
+        likes: 2300,
+        liked: false
+
+    },
+    {
+        img:postPic3,
+        name: "Sumera",
+        desc: "All the hard work you put into this piece really shows!",
+        likes: 800,
+        liked: false
+    }
+]
